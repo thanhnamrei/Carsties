5601739287529a7a5fc2fce0680adb9b2799ed97
Dương Thanh Nam <62831022+thanhnamrei@users.noreply.github.com>
2024-03-11 20:58:35 +0700

    feat: input

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,36 +1,85 @@
 "use client";
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function AuctionForm() {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
-  } = useForm();
+  } = useForm({
+    mode: 'onTouched'
+  });
+
+
+  useEffect(() => {
+    setFocus('make');
+  })
 
   function onSubmit(data: FieldValues) {
     console.log(data);
   }
   return (
     <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("make", { required: "Make is required" })}
-          placeholder="Make"
-          color={errors?.make && "failure"}
-          helperText={errors.make?.message as string}
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label="Color"
+        name="color"
+        control={control}
+        rules={{ required: "Color is required" }}
+      />
+
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Year"
+          name="year"
+          control={control}
+          type="number"
+          rules={{ required: "Year is required" }}
+        />
+        <Input
+          label="Mileage"
+          name="mileage"
+          control={control}
+          type="number"
+          rules={{ required: "Mileage is required" }}
         />
       </div>
+      <Input
+        label="Image Url"
+        name="imageUrl"
+        control={control}
+        rules={{ required: "Image Url is required" }}
+      />
 
-      <div className="mb-3 block">
-        <TextInput
-          {...register("model", { required: "Model is required" })}
-          placeholder="Model"
-          color={errors?.model && "failure"}
-          helperText={errors.modle?.message as string}
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Reserve Price (enter 0 if no reserve)"
+          name="reservePrice"
+          control={control}
+          type="number"
+          rules={{ required: "Year is required" }}
+        />
+        <Input
+          label="Auction end date/time"
+          name="auctionEnd"
+          control={control}
+          type="date"
+          rules={{ required: "Auction end date is required" }}
         />
       </div>
 
@@ -42,7 +91,7 @@ export default function AuctionForm() {
           outline
           color="success"
           isProcessing={isSubmitting}
-          disabled={!isValid}
+          // disabled={!isValid}
           type="submit"
         >
           Submit
